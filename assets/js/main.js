cd94e4363c4f1471b5157b438b1f2878f74c2435
oumaima <oumaymasghayerse@gmail.com>
2022-12-07 16:36:30 +0100

    add hmtl

---
+++ assets/js/main.js
@@ -0,0 +1,446 @@
+(function ($) {
+
+  "use strict";
+
+  /*
+  |--------------------------------------------------------------------------
+  | Template Name: Disfolio
+  | Author: DiscreteDev
+  | Version: 1.0.0
+  |--------------------------------------------------------------------------
+  |--------------------------------------------------------------------------
+  | TABLE OF CONTENTS:
+  |--------------------------------------------------------------------------
+  |
+  | 1. Placeholder
+  | 2. Dynamic Background
+  | 3. Menu
+  | 4. Sticky Header
+  | 5. One Page Navigation
+  | 6. Progress Bar
+  | 7. Ajax Contact Form And Appointment
+  | 8. Light Gallery
+  | 9. Social Button Hover
+  | 10. swiper Slider
+  | 11. particles
+  | 12. Ripple
+  | 13. Parallax Effect
+  */
+
+  /*--------------------------------------------------------------
+    Scripts initialization
+  --------------------------------------------------------------*/
+  $.exists = function (selector) {
+    return $(selector).length > 0;
+  };
+
+  $(window).on("load", function () {
+    $(window).trigger("scroll");
+    $(window).trigger("resize");
+    preloaderSetup();
+  });
+
+  $(document).on("ready", function () {
+    $(window).trigger("resize");
+    dynamicBackground();
+    progressBarInit();
+    stickyHeader();
+    onePageNavigation();
+    mainMenu();
+    lightGallery();
+    socialBtnHover();
+    particles();
+    parallaxEffect();
+    rippleInit();
+    new WOW().init();
+
+  });
+
+  $(window).on("scroll", function () {
+    stickyHeader();
+    parallaxEffect();
+  });
+
+  /*--------------------------------------------------------------
+    1. Placeholder
+  --------------------------------------------------------------*/
+  function preloaderSetup() {
+    $(".st-perloader").fadeOut();
+    $("st-perloader-in").delay(150).fadeOut("slow");
+  }
+
+  /*--------------------------------------------------------------
+    2. Dynamic Background
+  --------------------------------------------------------------*/
+  function dynamicBackground() {
+    // Background images
+    $('.st-dynamic-bg').each(function () {
+      var src = $(this).attr('data-src');
+      $(this).css({
+        'background-image': 'url(' + src + ')'
+      });
+    });
+  }
+
+  /*--------------------------------------------------------------
+    3. Menu
+  --------------------------------------------------------------*/
+  function mainMenu() {
+    $('.st-nav').append('<span class="st-munu-toggle"><span></span></span>');
+    $('.menu-item-has-children').append('<span class="st-munu-dropdown-toggle"></span>');
+    $('.st-munu-toggle').on('click', function () {
+      $(this).toggleClass("st-toggle-active").siblings('.st-nav-list').slideToggle();;
+    });
+    $('.st-munu-dropdown-toggle').on('click', function () {
+      $(this).toggleClass('active').siblings('ul').slideToggle();
+    });
+    //onclick menu item close menu in mobile devices
+    $(".st-nav-list li a").click(function () {
+      $(".st-nav-list").slideToggle();
+    });
+    $('.st-nav-list li a').on('click', function () {
+      $(".st-munu-toggle").removeClass("st-toggle-active");
+    });
+  }
+
+  /*--------------------------------------------------------------
+    4. Sticky Header
+  --------------------------------------------------------------*/
+  function stickyHeader() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10) {
+      $('.st-sticky-header').addClass('st-sticky-active');
+    } else {
+      $('.st-sticky-header').removeClass('st-sticky-active');
+    }
+  }
+
+  /*--------------------------------------------------------------
+    5. One Page Navigation
+  --------------------------------------------------------------*/
+  function onePageNavigation() {
+    // Click To Go Top
+    $('.st-smooth-move').on('click', function () {
+      var thisAttr = $(this).attr('href');
+      if ($(thisAttr).length) {
+        var scrollPoint = $(thisAttr).offset().top - 10;
+        $('body,html').animate({
+          scrollTop: scrollPoint
+        }, 50);
+      }
+      return false;
+    });
+
+    // One Page Active Class
+    var topLimit = 300,
+      ultimateOffset = 200;
+
+    $('.st-onepage-nav').each(function () {
+      var $this = $(this),
+        $parent = $this.parent(),
+        current = null,
+        $findLinks = $this.find("a");
+
+      function getHeader(top) {
+        var last = $findLinks.first();
+        if (top < topLimit) {
+          return last;
+        }
+        for (var i = 0; i < $findLinks.length; i++) {
+          var $link = $findLinks.eq(i),
+            href = $link.attr("href");
+
+          if (href.charAt(0) === "#" && href.length > 1) {
+            var $anchor = $(href).first();
+            if ($anchor.length > 0) {
+              var offset = $anchor.offset();
+              if (top < offset.top - ultimateOffset) {
+                return last;
+              }
+              last = $link;
+            }
+          }
+        }
+        return last;
+      }
+
+      $(window).on("scroll", function () {
+        var top = window.scrollY,
+          height = $this.outerHeight(),
+          max_bottom = $parent.offset().top + $parent.outerHeight(),
+          bottom = top + height + ultimateOffset;
+
+        var $current = getHeader(top);
+
+        if (current !== $current) {
+          $this.find(".active").removeClass("active");
+          $current.addClass("active");
+          current = $current;
+        }
+      });
+    });
+  }
+
+
+  /*--------------------------------------------------------------
+    6. Progress Bar
+  --------------------------------------------------------------*/
+  function progressBarInit() {
+    $('.st-progressbar').each(function () {
+      var progressPercentage = $(this).data('progress') + "%";
+      $(this).find('.st-progressbar-in').css('width', progressPercentage);
+    });
+  }
+
+
+  /*--------------------------------------------------------------
+    7. Ajax Contact Form And Appointment
+  --------------------------------------------------------------*/
+  //contact form js
+  $(function () {
+    // Get the form.
+    var form = $('#contact-form');
+    // Get the messages div.
+    var formMessages = $('.form-message');
+    // Set up an event listener for the contact form.
+    $(form).submit(function (e) {
+      // Stop the browser from submitting the form.
+      e.preventDefault();
+      // Serialize the form data.
+      var formData = $(form).serialize();
+      // Submit the form using AJAX.
+      $.ajax({
+          type: 'POST',
+          url: $(form).attr('action'),
+          data: formData
+        })
+        .done(function (response) {
+          // Make sure that the formMessages div has the 'success' class.
+          $(formMessages).removeClass('error');
+          $(formMessages).addClass('success');
+          // Set the message text.
+          $(formMessages).text(response);
+          // Clear the form.
+          $('#contact-form input, #contact-form textarea').val('');
+        })
+        .fail(function (data) {
+          // Make sure that the formMessages div has the 'problem' class.
+          $(formMessages).removeClass('success');
+          $(formMessages).addClass('problem');
+          // Set the message text.
+          if (data.responseText !== '') {
+            $(formMessages).text(data.responseText);
+          } else {
+            $(formMessages).text('Oops! An error occured and your message could not be sent.');
+          }
+        });
+    });
+  });
+
+  /*--------------------------------------------------------------
+    8. Light Gallery
+  --------------------------------------------------------------*/
+  function lightGallery() {
+    $('.st-lightgallery').each(function () {
+      $(this).lightGallery({
+        selector: '.st-lightbox-item',
+        subHtmlSelectorRelative: false,
+        thumbnail: false,
+        mousewheel: true
+      });
+    });
+  }
+
+  /*--------------------------------------------------------------
+    9. Social Button Hover
+  --------------------------------------------------------------*/
+  function socialBtnHover() {
+    $(".st-social-btn").hover(
+      function () {
+        $(this).addClass("active").siblings().removeClass('active');
+      }
+    )
+  }
+
+  /*--------------------------------------------------------------
+    10. review  Slider
+  --------------------------------------------------------------*/
+  var swiper = new Swiper(".st-reviews-wrap", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  });
+  /*--------------------------------------------------------------
+    11. particles
+  --------------------------------------------------------------*/
+  function particles() {
+    if ($.exists('#particles-js')) {
+      particlesJS("particles-js", {
+        "particles": {
+          "number": {
+            "value": 355,
+            "density": {
+              "enable": true,
+              "value_area": 789.1476416322727
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.48927153781200905,
+            "random": false,
+            "anim": {
+              "enable": true,
+              "speed": 0.6,
+              "opacity_min": 0,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 2,
+            "random": true,
+            "anim": {
+              "enable": true,
+              "speed": 5,
+              "size_min": 0,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 0.2,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "bubble"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 83.91608391608392,
+              "size": 1,
+              "duration": 3,
+              "opacity": 1,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      });
+    }
+  }
+  /*--------------------------------------------------------------
+    12. Ripple
+  --------------------------------------------------------------*/
+  function rippleInit() {
+    if ($.exists('.st-ripple-version')) {
+      $('.st-ripple-version').each(function () {
+        $('.st-ripple-version').ripples({
+          resolution: 512,
+          dropRadius: 20,
+          perturbance: 0.01,
+        });
+      });
+    }
+  }
+
+  /*--------------------------------------------------------------
+    13. Parallax Effect
+  --------------------------------------------------------------*/
+  function parallaxEffect() {
+    $('.st-parallax').each(function () {
+      var windowScroll = $(document).scrollTop(),
+        windowHeight = $(window).height(),
+        barOffset = $(this).offset().top,
+        barHeight = $(this).height(),
+        barScrollAtZero = windowScroll - barOffset + windowHeight,
+        barHeightWindowHeight = windowScroll + windowHeight,
+        barScrollUp = barOffset <= (windowScroll + windowHeight),
+        barSctollDown = barOffset + barHeight >= windowScroll;
+
+      if (barSctollDown && barScrollUp) {
+        var calculadedHeight = barHeightWindowHeight - barOffset;
+        var largeEffectPixel = ((calculadedHeight / 5));
+        var mediumEffectPixel = ((calculadedHeight / 20));
+        var miniEffectPixel = ((calculadedHeight / 10));
+
+        $(this).find('.st-to-left').css('transform', `translateX(-${miniEffectPixel}px)`);
+        $(this).find('.st-to-right').css('transform', `translateX(${miniEffectPixel}px)`);
+        $(this).css('background-position', `center -${largeEffectPixel}px`);
+      }
+    });
+  }
+})(jQuery); // End of use strict
